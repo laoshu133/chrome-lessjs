f6baf4b9d0618a23708ed1e6efc6e10a7fbe8dda
laoshu133 <admin@laoshu133.com>
2014-09-22 22:40:18 +0800

    grunt 配置更新

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,18 @@ module.exports = function(grunt) {
 		jshint: {
 			all: [ 'Gruntfile.js', 'js/*.js', '!js/less-*.js', '!js/source-map-*.js', '!js/*.min.js' ]
 		},
+		concat: {
+			dev: {
+				files: {
+					'js/source-map.min.js': ['js/source-map-header.js', 'js/source-map-*.js', 'js/source-map-footer.js']
+				}
+			}
+		},
 		uglify: {
 			dev: {
 				files: {
 					'js/less.min.js': ['js/less-*.js'],
-					'js/source-map.min.js': ['js/source-map-*.js']
+					'js/source-map.min.js': ['js/source-map.min.js']
 				}
 			}
 		}
@@ -22,9 +29,10 @@ module.exports = function(grunt) {
 
 	// 载入任务
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// 声明别名
-	grunt.registerTask('default', ['jshint', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 	grunt.registerTask('dev', ['default']);
 };
